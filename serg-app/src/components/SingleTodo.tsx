8b5d8f7a81ff3afbefef72943bb7dca86e52e6ae
Sergeypervak <har4enko1983@gmail.com>
2022-05-23 14:32:44 +0300

    done, delete

--- serg-app/src/components/SingleTodo.tsx
+++ serg-app/src/components/SingleTodo.tsx
@@ -3,8 +3,9 @@ import { Todo } from './model';
 import { AiFillEdit,AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import "./styles.css";
+import TodoList from './TodoList';
 
-type Props = {
+interface Props {
  todo:Todo,
  todos: Todo[],
  setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
@@ -12,20 +13,49 @@ type Props = {
 
 }
 
-const SingleTodo = ({todo, todos, setTodos}) => {
-  return <form className='todos__single'>
-      <span className="todos__single--text">{todo.todo}</span>
+  const SingleTodo = ({todo, todos,setTodos}:Props) => {
 
+  // const [edit, setEdit] = useState<boolean>(false);
+  
+  // const [editTodo, setEditTodo] = useState<string>(todo.todo);
+  
+  const handleDone = (id:number) => {
+ setTodos(
+   todos.map((todo)=>
+                   todo.id===id?{...todo, isDone: !todo.isDone}: todo
+      )
+    );
+  };
+
+  const handleDelete = (id:number) => {
+   setTodos(todos.filter((todo)=>todo.id !== id))
+  }
+
+
+  return <form className='todos__single'>
+      {
+        todo.isDone? (
+      <s className="todos__single--text">{todo.todo}</s>
+        ) : (
+          <span className="todos__single--text">{todo.todo}</span>
+        )
+}
 <div>
-    <span className="icon">
+    <span className="icon" 
+    onClick= {() => {
+ if(!edit && todo.isDone) {
+ setEdit(!edit)
+ }
+}}
+>
     <AiFillEdit />
     </span>
 
-    <span className="icon">
+    <span className="icon"onClick={()=>handleDelete(todo.id)}>
     < AiFillDelete />  
     </span>
 
-    <span className="icon">
+    <span className="icon" onClick={()=>handleDone(todo.id)}>
         <MdDone/>
     </span>
 </div>
@@ -33,3 +63,7 @@ const SingleTodo = ({todo, todos, setTodos}) => {
   };
 
 export default SingleTodo;
+
+function useState<T>(arg0: boolean): [any, any] {
+  throw new Error('Function not implemented.');
+}
